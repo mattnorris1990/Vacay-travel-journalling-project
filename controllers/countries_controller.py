e8b4d29cda34d47da85953adba14afcbb4d14a0b
mattnorris1990 <mattnorris1990@gmail.com>
2022-04-24 15:36:36 +0100

    added add_country fuctionality to front end with placeholder image insert logic in controller

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -9,3 +9,20 @@ countries_blueprint = Blueprint("countries", __name__)
 def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", countries = countries)
+
+@countries_blueprint.route("/addcountry")
+def add_country_page():
+    return render_template('countries/add_country.html')
+
+@countries_blueprint.route("/addcountry", methods = ['POST'])
+def add_country():
+    name = request.form['country_name']
+    if len(request.form['image']) > 0:
+        image = request.form['image']
+    else:
+        image = "images/placeholder_country_image.jpg"
+    
+    country_object = Country(name, image)
+    country_repository.save(country_object)
+
+    return redirect("/")
