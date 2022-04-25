c6a917db6a2397983136a42c981ec3c959dfe898
mattnorris1990 <mattnorris1990@gmail.com>
2022-04-25 16:26:01 +0100

    added homepage in controllers and templates, began some css

--- console.py
+++ console.py
@@ -8,19 +8,60 @@ import repositories.place_repository as place_repository
 import repositories.country_entry_repository as country_entry_repository
 import repositories.place_entry_repository as place_entry_repository
 
-# country_entry_repository.delete_all()
-# place_repository.delete_all()
-# country_repository.delete_all()
+country_entry_repository.delete_all()
+place_repository.delete_all()
+country_repository.delete_all()
+
+# TEST COUNTRIES
+
+country1 = Country("France", "images/placeholder_country_image.jpg")
+country_repository.save(country1)
+
+country2 = Country("Spain", "images/placeholder_country_image.jpg")
+country_repository.save(country2)
+
+country3 = Country("Germany", "images/placeholder_country_image.jpg")
+country_repository.save(country3)
+
+country4 = Country("USA", "images/placeholder_country_image.jpg")
+country_repository.save(country4)
+
+country5 = Country("Bulgaria", "images/placeholder_country_image.jpg")
+country_repository.save(country5)
+
+# TEST PLACES
+
+place1 = Place("Paris", country1, "images/placeholder_country_image.jpg")
+place_repository.save(place1)
+
+place2 = Place("Lyon", country1, "images/placeholder_country_image.jpg")
+place_repository.save(place2)
+
+place3 = Place("Madrid", country2, "images/placeholder_country_image.jpg")
+place_repository.save(place3)
+
+place4 = Place("Barcelona", country2, "images/placeholder_country_image.jpg")
+place_repository.save(place4)
+
+place5 = Place("Berlin", country3, "images/placeholder_country_image.jpg")
+place_repository.save(place5)
+
+place6 = Place("Hamburg", country3, "images/placeholder_country_image.jpg")
+place_repository.save(place6)
+
+place7 = Place("New York", country4, "images/placeholder_country_image.jpg")
+place_repository.save(place7)
+
+place8 = Place("Los Angeles", country4, "images/placeholder_country_image.jpg")
+place_repository.save(place8)
+
+place9 = Place("Sofia", country5, "images/placeholder_country_image.jpg")
+place_repository.save(place9)
+
+place10 = Place("Varna", country5, "images/placeholder_country_image.jpg")
+place_repository.save(place10)
 
 
-# country1 = Country("France", "images/placeholder_country_image.jpg")
-# country_repository.save(country1)
-
-# country2 = Country("Spain", "images/placeholder_country_image.jpg")
-# country_repository.save(country2)
-
-# country3 = Country("Germany", "images/placeholder_country_image.jpg", True)
-# country_repository.save(country3)
 
 # countries = country_repository.select_all()
 
@@ -36,14 +77,6 @@ import repositories.place_entry_repository as place_entry_repository
 # country3 = Country("Italy", "images/placeholder_country_image.jpg", "True", 3)
 # country_repository.update(country3)
 
-# place1 = Place("Paris", country1, "images/placeholder_country_image.jpg")
-# place_repository.save(place1)
-
-# place2 = Place("Berlin", country3, "images/placeholder_country_image.jpg")
-# place_repository.save(place2)
-
-# place3 = Place("Hamburg", country3, "images/placeholder_country_image.jpg")
-# place_repository.save(place3)
 
 # places = place_repository.select_all()
 
@@ -87,7 +120,7 @@ import repositories.place_entry_repository as place_entry_repository
 # for entry in entries:
 #     print(entry.__dict__)
 
-entry = place_entry_repository.select(1)
+# entry = place_entry_repository.select(1)
 
-print(entry.__dict__)
+# print(entry.__dict__)
 
---
+++ controllers/controller.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, Blueprint
+
+home_blueprint = Blueprint("/", __name__)
+
+@home_blueprint.route("/")
+def home_page():
+    return render_template('index.html')
